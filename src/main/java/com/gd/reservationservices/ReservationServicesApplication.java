09854997822e9524d549cd13ae451115fbef6156
summerr0-0 <summerr060@gmail.com>
2023-06-18 14:05:52 +0900

    first init

---
+++ src/main/java/com/gd/reservationservices/ReservationServicesApplication.java
@@ -0,0 +1,13 @@
+package com.gd.reservationservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gd/reservationservices/ReservationServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gd.reservationservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
