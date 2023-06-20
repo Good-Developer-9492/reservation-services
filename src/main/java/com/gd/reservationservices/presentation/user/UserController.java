dcf491d2a806ee1506ecfe3a6c96b9126a5f9aa0
hyunho058 <hyunho058@naver.com>
2023-06-20 22:02:55 +0900

    사용자 조회 응답 객체 부 생성자 사용, 사용자 조회 메서드 명 변경

--- src/main/java/com/gd/reservationservices/presentation/user/UserController.java
+++ src/main/java/com/gd/reservationservices/presentation/user/UserController.java
@@ -4,6 +4,7 @@ import com.gd.reservationservices.application.user.UserService;
 import com.gd.reservationservices.application.user.dto.SearchUser;
 import com.gd.reservationservices.common.response.EmptyResponse;
 import com.gd.reservationservices.common.response.SingleResponse;
+import com.gd.reservationservices.presentation.user.request.CreateUserRequest;
 import com.gd.reservationservices.presentation.user.response.SearchUserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -21,17 +22,10 @@ public class UserController {
     }
 
     @GetMapping("/users/{id}")
-    public SingleResponse<SearchUserResponse> join(@PathVariable Long id) {
+    public SingleResponse<SearchUserResponse> searchUser(@PathVariable Long id) {
         SearchUser searchUser = userService.searchUser(id);
 
-        SearchUserResponse searchUserResponse =
-            new SearchUserResponse(
-                searchUser.userId(),
-                searchUser.name(),
-                searchUser.age(),
-                searchUser.email(),
-                searchUser.phone(),
-                searchUser.role());
+        SearchUserResponse searchUserResponse = new SearchUserResponse(searchUser);
 
         return new SingleResponse.Ok<>(searchUserResponse);
     }
--- src/main/java/com/gd/reservationservices/presentation/user/response/SearchUserResponse.java
+++ src/main/java/com/gd/reservationservices/presentation/user/response/SearchUserResponse.java
@@ -1,5 +1,6 @@
 package com.gd.reservationservices.presentation.user.response;
 
+import com.gd.reservationservices.application.user.dto.SearchUser;
 import com.gd.reservationservices.infrastructure.user.value.Role;
 
 public record SearchUserResponse(
@@ -10,4 +11,14 @@ public record SearchUserResponse(
     String phone,
     Role role
 ) {
+    public SearchUserResponse(SearchUser searchUser) {
+        this(
+            searchUser.userId(),
+            searchUser.name(),
+            searchUser.age(),
+            searchUser.email(),
+            searchUser.phone(),
+            searchUser.role()
+        );
+    }
 }
