f36c6a9ba90e249ad6a872eefc822996ca04a63b
Hyunho Kim <hyunho058@naver.com>
2023-07-15 13:23:29 +0900

    사용자 정보 수정 api path 수정 (#22)

--- src/main/java/com/gd/reservationservices/presentation/user/reqeust/UpdateUserRequest.java
+++ src/main/java/com/gd/reservationservices/presentation/user/reqeust/UpdateUserRequest.java
@@ -3,14 +3,12 @@ package com.gd.reservationservices.presentation.user.reqeust;
 import com.gd.reservationservices.application.user.dto.UpdateUserCommend;
 
 public record UpdateUserRequest(
-    Long id,
     String userPw,
     String name,
     int age
 ) {
     public UpdateUserCommend toValue() {
         return new UpdateUserCommend(
-            this.id,
             this.userPw,
             this.name,
             this.age
